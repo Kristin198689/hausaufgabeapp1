555be35db04132c8ad81dcaf5fe680e9627ea4a7
Kristin198689 <163996836+Kristin198689@users.noreply.github.com>
2024-07-30 13:11:03 +0200

    retern

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -5,9 +5,7 @@ function ImageComponent() {
         className="Pictuers"
         height="250"
         width="250"
-        src="https://img.freepik.com/free-photo/photorealistic-happiness-scene-with-happy-woman_23-2151423540.jpg?uid=R137466982&ga=GA1.1.1913715299.1710867567"
-        alt="sun"
-      ></img>
+        src="https://lms.itcareerhub.de/pluginfile.php/1/theme_universe/customlogo/1721818326/Group_3793.svg"></img>
     );
   }
   
